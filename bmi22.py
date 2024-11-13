aa5219aa94a284e1570e9e0cc658c8f059927829
Jaeson <jaesonmathew.22@ichat.sp.edu.sg>
2024-11-13 14:51:52 +0800

    BMI function

---
+++ bmi22.py
@@ -0,0 +1,22 @@
+print("This is my dummy code for the lab test")
+
+
+def calculate_bmi(height, weight):
+    print("Height = " + str(height))
+    print("Weight = " + str(weight))
+
+    bmi = weight/height**2
+    format_bmi="{:.2f}".format(bmi)
+
+    print("Your BMI is = "+ str(format_bmi))
+
+    if bmi<18.5:
+        classification = "Under Weight"
+    if bmi>=18.5 and bmi<=25.0:
+        classification = "Normal Weight"
+    else:
+        classification = "Over Weight"
+    
+    print("Your BMI classification is= "+ classification)
+
+calculate_bmi(height=1.73,weight=53)
