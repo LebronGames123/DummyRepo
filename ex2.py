11ef08af5a83422cbcdb8ec44af93a93101eaf70
Jaeson <jaesonmathew.22@ichat.sp.edu.sg>
2024-11-13 15:55:29 +0800

    Ex2Commit

---
+++ ex2.py
@@ -0,0 +1,10 @@
+def display_main_menu():
+    user_input=input("Enter some numbers separated by commas (e.g. 5, 67,32)")
+
+
+
+def calc_avg(array):
+    avg=sum(user_inp)/len(user_inp)
+    print("The average= " + avg)
+
+
